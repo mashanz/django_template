d904dcd4b903a0d9f1892d310eef2024be949150
Rohmad Eko Wahyudi <rohmad.ew@gmail.com>
2024-03-19 08:07:21 +0700

    fix: wording

--- app/app/utils/Pagination.py
+++ app/app/utils/Pagination.py
@@ -2,15 +2,15 @@
 from django.core.paginator import Paginator
 
 # This function is used to paginate the data
-# baseUrlPaginator: The URL of the paginator    
+# baseUrlPagination: The URL of the paginator    
 # countOfAllData: The total number of data
 # pageNumber: The current page number
 # limit: The number of data to be displayed per page
-def paginator(baseUrlPaginator, countOfAllData, pageNumber, limit):
+def paginator(baseUrlPagination, countOfAllData, pageNumber, limit):
     paginator = Paginator([], limit)
     paginator.count = countOfAllData
     page = paginator.get_page(pageNumber)
 
     startNumber = (page.number - 1) * limit
 
-    return {"page": page, "startNumber": startNumber, "limit": limit, "baseUrlPaginator": baseUrlPaginator}
+    return {"page": page, "startNumber": startNumber, "limit": limit, "baseUrlPagination": baseUrlPagination}
